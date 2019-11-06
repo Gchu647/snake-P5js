46475eaad198d90b26a372cd4cb5f3fe9ca62859
George <g7chu@Temps-MBP.hawaii.rr.com>
2019-11-06 06:39:26 -1000

    changes color everytime I press the keys W, A, S, D

--- sketch.js
+++ sketch.js
@@ -1,13 +1,29 @@
+let value = '#ffffff';
+
 function setup() {
   createCanvas(640, 480);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
+  fill(value);
+  rect(25, 25, 50, 50);
+}
+function keyTyped() {
+  switch (key) {
+    case 'w':
+      value = '#fcba03';
+      break;
+    case 'a':
+      value = '#0ee375';
+      break;
+    case 's':
+        value = '#0e87e3';
+        break;
+    case 'd':
+        value = '#e34a0e';
+        break;
+    default:
+      value = '#ffffff';
+      break;
   }
-
-  square(mouseX, mouseY, 80);
 }
