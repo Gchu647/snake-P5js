b72bac8ebf4f005630f6a2bff979ee8212a156ae
George <g7chu@Temps-MBP.hawaii.rr.com>
2019-11-06 01:15:37 -1000

    a moving rectangle

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
 function setup() {
-
+  createCanvas(640, 480);
 }
 
 function draw() {
-  ellipse(50, 50, 80, 80);
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+
+  square(mouseX, mouseY, 80);
 }
