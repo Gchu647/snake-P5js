c2dd5aae838823adb2c7015a0709667f378ced21
George <g7chu@Temps-MBP.hawaii.rr.com>
2019-11-11 00:36:40 -1000

    only loop when D is pressed

--- sketch.js
+++ sketch.js
@@ -4,29 +4,39 @@ let y = 200;
 
 function setup() {
   createCanvas(500, 500);
+  noLoop();
 }
 
 function draw() {
   background(220);
   fill(0);
-  rect(x, y, 50, 50);
+  rect(x, y, 20, 20);
   x = x + 1;
 }
+
 function keyTyped() {
-  switch (key) {
-    case 'w':
-      y = y - 20;
-      break;
-    case 's':
-      y = y + 20;
-      break;
-    case 'a':
-      x = x - 20;
-      break;
-    case 'd':
-      x = x + 20;
-      break;
-    default:
-      break;
+  if (key === 'd') {
+    loop();
+  } else {
+    noLoop();
   }
 }
+
+// function keyTyped() {
+//   switch (key) {
+//     case 'w':
+//       y = y - 20;
+//       break;
+//     case 's':
+//       y = y + 20;
+//       break;
+//     case 'a':
+//       x = x - 20;
+//       break;
+//     case 'd':
+//       x = x + 20;
+//       break;
+//     default:
+//       break;
+//   }
+// }
