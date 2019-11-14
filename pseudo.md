6dc341a77a2a7753fc64f08113da7d2cf8930079
George <g7chu@Temps-MBP.hawaii.rr.com>
2019-11-14 08:23:46 -1000

    red square moves when touch by black square

--- pseudo.md
+++ pseudo.md
@@ -1,7 +1,8 @@
-##Steps
-- Setup noLoop so draw only functions once. Then setup a reDraw loop inside keyTyped so it will contiously go in one directions.
-
 https://p5js.org/reference/#/p5/noLoop
 
 ##Next Steps
 - Make a red square randomly re-appear within the canvas everytime the black square touches it.
+- Apply intersection stuff to sketch.
+
+##Steps
+- if distance is less or equal to the radius of two square, activate if condition.
--- sketch.js
+++ sketch.js
@@ -1,14 +1,21 @@
-let x = 150;
-let y = 200;
+let x = 250;
+let y = 250;
+let x2 = Math.floor(Math.random() * 470);
+let y2 = Math.floor(Math.random() * 470);
+let squareRadius = 20;
+let firstLoop = true;
 
 
 function setup() {
   createCanvas(500, 500);
   noLoop();
+  frameRate(180);
 }
 
 function draw() {
   background(220);
+  // black square code
+  rectMode(CENTER);
   fill(0);
   rect(x, y, 20, 20);
 
@@ -28,6 +35,20 @@ function draw() {
     default:
       break;
   }
+  
+  // red square code
+  rectMode(CENTER);
+  fill('#f54242');
+  rect(x2, y2, 20, 20);
+
+  // move red square if d is too close
+  let d = dist(x, y, x2, y2);
+  console.log('distance: ', d);
+  
+  if (d <= squareRadius) {
+    x2 = Math.floor(Math.random() * 500);
+    y2 = Math.floor(Math.random() * 500);
+  }
 }
 
 function keyTyped() {
@@ -37,22 +58,3 @@ function keyTyped() {
     noLoop();
   }
 }
-
-// function keyTyped() {
-//   switch (key) {
-//     case 'w':
-//       y = y - 20;
-//       break;
-//     case 's':
-//       y = y + 20;
-//       break;
-//     case 'a':
-//       x = x - 20;
-//       break;
-//     case 'd':
-//       x = x + 20;
-//       break;
-//     default:
-//       break;
-//   }
-// }
