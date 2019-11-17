ba943c03cd97d09f3401403070c2427b819d88c3
George <g7chu@Temps-MBP.hawaii.rr.com>
2019-11-16 14:57:53 -1000

    4 boxes snake eats 1 red box

--- pseudo.md
+++ pseudo.md
@@ -1,4 +1,5 @@
 https://p5js.org/reference/#/p5/noLoop
 
 ##Next Steps
-- Array objects.
+- Add a new Box() and increase boxes[] everytime there the red box is touched.
+- Watch Coding Train to understand Class.
--- sketch.js
+++ sketch.js
@@ -1,17 +1,37 @@
-let squareRadius = 20;
-let addSquare = 0; // will use for later
+let squareRadius = 10;
 let boxes = [];
 let boxIndex = 2;
 
 function setup() {
   createCanvas(500, 500);
+  frameRate(25);
+  // intial boxes
+  boxes[0] = new Box(null, null, '#f54242'); // red box
+  boxes[1] = new Box(230, 240); // 1st white box
+
+  //trailing boxes
+  boxes[2] = new Box(240, 240);
+  boxes[3] = new Box(250, 240);
+  boxes[4] = new Box(250, 250);
+
   noLoop();
-  // make black square and red square
-  box1 = new Box(0);
-  box2 = new Box('#f54242');
-  //put boxes in an array;
-  boxes[0] = box1;
-  boxes[1] = box2;
+}
+
+function draw() {
+  background(220);
+  //1st white box code
+  boxes[1].display();
+  boxes[1].move();
+  
+  // red box code
+  boxes[0].display();
+  boxes[0].teleport(boxes[1].x, boxes[1].y);
+
+  // Display trailing boxes
+  for (let i = 2; i < boxes.length; i++) {
+    boxes[i].display();
+    boxes[i].follow(boxes[i-1].prevX, boxes[i-1].prevY);
+  }
 }
 
 function keyTyped() {
@@ -22,71 +42,63 @@ function keyTyped() {
   }
 }
 
-function draw() {
-  background(220);
-  // black square code
-  box1.display();
-  box1.move();
-  
-  // red square code
-  box2.display();
-  box2.teleport(box1.x, box1.y);
-
-  // if more than 2 boxes
-  // TEST: display stores boxes from 2 and beyond
-  console.log('boxes: ', boxes.length);
-  for (let i = 2; i < boxes.length; i++) {
-    boxes[i].display();
-  }
-}
-
 class Box {
-  constructor (color) {
-    this.x = Math.floor(Math.random() * 400);
-    this.y = Math.floor(Math.random() * 400);
-    this.width = 20;
-    this.height = 20;
-    console.log(color);
-    this.color = color;
+  constructor (x, y, color) {
+    this.x = x || Math.floor(Math.random() * 400);
+    this.y = y || Math.floor(Math.random() * 400);
+    this.width = 10;
+    this.height = 10;
+    this.color = color || 255;
+
+    // for tracking
+    this.prevX = 0;
+    this.prevY = 0;
   }
 
-  x () {
-    return this.x;
+  track() {
+    this.prevX = this.x;
+    this.prevY = this.y;
   }
 
-  y () {
-    return this.y;
-  }
+  move () { //movement of 1st box
+    this.track();
 
-  move () {
     switch (key) { // moves square in a different direction
       case 'w':
-        this.y = this.y - 3;
+        this.y = this.y - 10;
         break;
       case 's':
-        this.y = this.y + 3;
+        this.y = this.y + 10;
         break;
       case 'a':
-        this.x = this.x - 3;
+        this.x = this.x - 10;
         break;
       case 'd':
-        this.x = this.x + 3;
+        this.x = this.x + 10;
         break;
       default:
         break;
     }
   }
 
+  follow(x2, y2) {
+    this.track();
+
+    // move
+    this.x = x2;
+    this.y = y2;
+  }
+
   teleport(x2, y2) {
     let d = dist(this.x, this.y, x2, y2);
   
-    if (d <= squareRadius) { // when box1 touch box2
+    if (d <= squareRadius) { // when boxes[1] touch boxes[0]
       this.x = Math.floor(Math.random() * 500);
       this.y = Math.floor(Math.random() * 500);
 
-      // TEST: Store new Box()
-      boxes[boxIndex] = new Box(100);
-      boxIndex ++
+      // TEST!!!!!
+      // boxes[boxIndex] = new Box(100);
+      // boxIndex ++
     }
   }
 
